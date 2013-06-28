54181afc73b8e0c0a3da40a4ef7cca4be4dc2d56
LowPowerLab <flexiboiu@gmail.com>
2013-06-28 16:13:38 -0400

    Adjust RF settings

--- RFM69.cpp
+++ RFM69.cpp
@@ -15,8 +15,8 @@ bool RFM69::initialize(byte freqBand, byte nodeID, byte networkID)
   {
     /* 0x01 */ { REG_OPMODE, RF_OPMODE_SEQUENCER_ON | RF_OPMODE_LISTEN_OFF | RF_OPMODE_STANDBY},
     /* 0x02 */ { REG_DATAMODUL, RF_DATAMODUL_DATAMODE_PACKET | RF_DATAMODUL_MODULATIONTYPE_FSK | RF_DATAMODUL_MODULATIONSHAPING_00}, //gaussian, bt=1.0
-    /* 0x03 */ { REG_BITRATEMSB, RF_BITRATEMSB_50000}, //default is 4.8 KBPS
-    /* 0x04 */ { REG_BITRATELSB, RF_BITRATELSB_50000},
+    /* 0x03 */ { REG_BITRATEMSB, RF_BITRATEMSB_55555}, //default is 4.8 KBPS
+    /* 0x04 */ { REG_BITRATELSB, RF_BITRATELSB_55555},
     /* 0x05 */ { REG_FDEVMSB, RF_FDEVMSB_50000}, //default is 5khz
     /* 0x06 */ { REG_FDEVLSB, RF_FDEVLSB_50000},
 
@@ -30,7 +30,7 @@ bool RFM69::initialize(byte freqBand, byte nodeID, byte networkID)
     // +17dBm formula: Pout=-14+OutputPower (with PA1 and PA2)**
     // +20dBm formula: Pout=-11+OutputPower (with PA1 and PA2)** and high power PA settings (section 3.3.7 in datasheet)
     ///* 0x11 */ { REG_PALEVEL, RF_PALEVEL_PA0_ON | RF_PALEVEL_PA1_OFF | RF_PALEVEL_PA2_OFF | RF_PALEVEL_OUTPUTPOWER_11111},
-    /* 0x19 */ { REG_RXBW, RF_RXBW_DCCFREQ_010 | RF_RXBW_MANT_24 | RF_RXBW_EXP_2},
+    /* 0x19 */ { REG_RXBW, RF_RXBW_DCCFREQ_010 | RF_RXBW_MANT_16 | RF_RXBW_EXP_2},
     /* 0x25 */ { REG_DIOMAPPING1, RF_DIOMAPPING1_DIO0_01}, //DIO0 is the only IRQ we're using
     /* 0x29 */ { REG_RSSITHRESH, 220}, // must be set to dBm = (-Sensitivity / 2) - default is 0xE4=228 so -114dBm
     ///* 0x2d */ { REG_PREAMBLELSB, RF_PREAMBLESIZE_LSB_VALUE } // default 3 preamble bytes 0xAAAAAA

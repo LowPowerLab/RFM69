dbc74430b8678094827a2ab780ebc5324d8cb0dd
LowPowerLab <flexiboiu@gmail.com>
2013-08-15 21:40:56 -0400

    Update Node.ino

--- Examples/Node/Node.ino
+++ Examples/Node/Node.ino
@@ -21,7 +21,7 @@ RFM69 radio;
 void setup() {
   Serial.begin(SERIAL_BAUD);
   radio.initialize(FREQUENCY,NODEID,NETWORKID);
-  //radio.setHighPower(); //uncomment only for RFM69HW!
+  //radio.setHighPower(); //must uncomment for RFM69HW!
   radio.encrypt(KEY);
   char buff[50];
   sprintf(buff, "\nTransmitting at %d Mhz...", FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);

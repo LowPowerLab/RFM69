c01ae0bf4e74f0516d4632fe64ae7fc3b543d4a9
Felix Rusu <felix@lowpowerlab.com>
2015-03-17 15:50:58 -0400

    Update Gateway.ino

--- Examples/Gateway/Gateway.ino
+++ Examples/Gateway/Gateway.ino
@@ -16,7 +16,6 @@
 //#define FREQUENCY     RF69_915MHZ
 #define ENCRYPTKEY    "sampleEncryptKey" //exactly the same 16 characters/bytes on all nodes!
 //#define IS_RFM69HW    //uncomment only for RFM69HW! Leave out if you have RFM69W!
-#define ACK_TIME      30 // max # of ms to wait for an ack
 #define SERIAL_BAUD   115200
 
 #ifdef __AVR_ATmega1284P__

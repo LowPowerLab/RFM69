372dd4a782da92e189e9f628e3bd1922a5869267
Ricardo Mena C <rgacort12@gmail.com>
2016-03-29 14:24:17 -0600

    Add __arm__ platform for SAMD21

--- RFM69.h
+++ RFM69.h
@@ -45,7 +45,7 @@
 #elif defined(__AVR_ATmega32U4__)
   #define RF69_IRQ_PIN          3
   #define RF69_IRQ_NUM          0
-#elif defined(__arm__)
+#elif defined(__arm__)//Use pin 10 or any pin you want
   #define RF69_IRQ_PIN          10
   #define RF69_IRQ_NUM          10
 #else 

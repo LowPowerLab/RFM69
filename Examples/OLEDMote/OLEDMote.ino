58f0288675a395b514940a449fb140f366b24c1f
LowPowerLab <felix@lowpowerlab.com>
2017-02-03 11:56:20 -0500

    minor additions

--- Examples/OLEDMote/OLEDMote.ino
+++ Examples/OLEDMote/OLEDMote.ino
@@ -29,6 +29,7 @@
 // and copyright notices in any redistribution of this code
 // **********************************************************************************
 #include <RFM69.h>    //get it here: https://www.github.com/lowpowerlab/rfm69
+#include <SPIFlash.h> //get library from: https://www.github.com/lowpowerlab/spiflash
 #include <LowPower.h> //get library from: https://github.com/lowpowerlab/lowpower
 #include "U8glib.h"   //get library from: https://code.google.com/p/u8glib/
 #include <SPI.h>      //included with Arduino IDE (www.arduino.cc)

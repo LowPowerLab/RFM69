2efd5b9b9fadb4de7d263dad0feac6fce5c52525
Max Prokhorov <prokhorov.max@outlook.com>
2021-01-26 22:24:22 +0300

    Move back the RF69_SPI_CS line

--- RFM69.h
+++ RFM69.h
@@ -118,6 +118,8 @@
 #endif
 ////////////////////////////////////////////////////
 
+#define RF69_SPI_CS             SS // SS is the SPI slave select pin, for instance D10 on ATmega328
+
 // INT0 on AVRs should be connected to RFM69's DIO0 (ex on ATmega328 it's D2, on ATmega644/1284 it's D2)
 #if defined(__AVR_ATmega168__) || defined(__AVR_ATmega328P__) || defined(__AVR_ATmega88) || defined(__AVR_ATmega8__) || defined(__AVR_ATmega88__)
   #define RF69_IRQ_PIN          2
@@ -141,8 +143,6 @@
 #else
   #define RF69_IRQ_PIN          2
 #endif
-
-#define RF69_SPI_CS             SS // SS is the SPI slave select pin, for instance D10 on ATmega328
 
 #define RF69_MAX_DATA_LEN       61 // to take advantage of the built in AES/CRC we want to limit the frame size to the internal FIFO size (66 bytes - 3 bytes overhead - 2 bytes crc)
 #define CSMA_LIMIT              -90 // upper RX signal sensitivity threshold in dBm for carrier sense access

e11dc1e0598be21cdf83a6587a3d7bf41f96b056
Jens Dede <jd@comnets.uni-bremen.de>
2021-07-15 22:33:27 +0200

    Fix for esp8266 ACK problem. Solution as suggested in #49 and #105

--- RFM69.cpp
+++ RFM69.cpp
@@ -304,7 +304,12 @@ void RFM69::sendACK(const void* buffer, uint8_t bufferSize) {
   int16_t _RSSI = RSSI; // save payload received RSSI value
   writeReg(REG_PACKETCONFIG2, (readReg(REG_PACKETCONFIG2) & 0xFB) | RF_PACKET2_RXRESTART); // avoid RX deadlocks
   uint32_t now = millis();
-  while (!canSend() && millis() - now < RF69_CSMA_LIMIT_MS) receiveDone();
+  while (!canSend() && millis() - now < RF69_CSMA_LIMIT_MS){
+      receiveDone();
+#ifdef ESP8266
+      delay(1); // Give esp8266-based boards to handle background tasks
+#endif
+  }
   SENDERID = sender;    // TWS: Restore SenderID after it gets wiped out by receiveDone()
   sendFrame(sender, buffer, bufferSize, false, true);
   RSSI = _RSSI; // restore payload RSSI
